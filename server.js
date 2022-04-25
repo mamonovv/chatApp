6f30db64879a4d18b7c9ab2f5e8348424c614f9d
mamonovv <mmvvlad@gmail.com>
2022-04-25 23:46:03 +0700

    add a sockets with message when users connect/disconnect

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const path = require('path')
 const express = require('express')
+const http = require('http')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 //Set static folder
 app.use(express.static(path.join(__dirname, 'assets')))
-console.log(path)
+
+//Run when client connects
+io.on('connection', socket => {
+    //Welcome current user
+    socket.emit('message', 'Welcome to ChatCord!')
+
+    //Broadcast when a user connects
+    socket.broadcast.emit('message', 'A user has joined to chat');
+
+    //Runs when client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat')
+    })
+})
 
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
